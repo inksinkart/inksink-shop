4072999c0c3db4994d4b97ca9f24e8a030a22f0b
Adarsh Poojari <adarsh@inksink.store>
2025-06-20 15:30:13 +0530

    Create omni.js

---
+++ omni.js
@@ -0,0 +1,8 @@
+window.omnisend = window.omnisend || [];
+omnisend.push(["brandID", "6853bf0968f5ed9fec5474b3"]);
+omnisend.push(["track", "$pageViewed"]);
+!function(){var e=document.createElement("script");
+e.type="text/javascript",e.async=!0,
+e.src="https://omnisnippet1.com/inshop/launcher-v2.js";
+var t=document.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(e,t)}();
